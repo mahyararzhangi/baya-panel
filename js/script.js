27fb4b0a4c154747941afc4e919fd6129f87e68c
Mahyar Arzhangi <mahyar.9069@yahoo.com>
2020-12-18 23:59:00 +0330

    baya employee panel created

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var employee = {
+    name:'مهیار',
+    family:'ارژنگی',
+    identityNumber:'0011948868',
+    phoneNumber:'09124137293',
+    employeeNumber:'318',
+    location:'ستاد',
+    startDate:'1399/09/05',
+    endDate:'0000/00/00',
+}
+
+console.log(employee);
